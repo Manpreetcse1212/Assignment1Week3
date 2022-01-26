5c66316943773fcc91ee397112ba9f3155f802ef
Manpreetcse1212 <87387032+Manpreetcse1212@users.noreply.github.com>
2022-01-26 12:07:17 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Assignment1Week3
+ 
---
+++ src/main/java/Assignment1.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author hp world
+ *
+ */
+public class Assignment1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
