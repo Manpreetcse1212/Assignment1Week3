6927bd25e4a4cceb2572cf007097de2156ca7644
Manpreetcse1212 <87387032+Manpreetcse1212@users.noreply.github.com>
2022-01-27 16:40:46 -0500

    Project Created 1

---
+++ src/main/java/com/D/week3/assignment1/GroupDAssignment1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.D.week3.assignment1;
+
+/**
+ * @author hp world
+ *
+ */
+public class GroupDAssignment1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
