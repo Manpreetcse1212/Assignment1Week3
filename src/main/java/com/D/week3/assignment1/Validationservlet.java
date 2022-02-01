106f0e651498b3f293227fc098764f109705752a
Bhumika5297 <32120089+Bhumika5297@users.noreply.github.com>
2022-01-31 22:39:50 -0500

    Update Validationservlet.java
    Commit done by Bhumika 565

--- src/main/java/com/D/week3/assignment1/Validationservlet.java
+++ src/main/java/com/D/week3/assignment1/Validationservlet.java
@@ -1,21 +1,37 @@
 package com.D.week3.assignment1;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
 
 /**
- * Servlet implementation class Validationservlet
+ * Date: 31.01.2022
+ * Group D 
+ * Member1: Manpreet kaur
+ * Member2: Manpreet Kaur
+ * Member3: Bhumikaben Manubhai Patel
+ * Member4: Ashikkumar Nareshbhai Patel
+ * Member5: Hardeep Kaur Chahal 
  */
 @WebServlet("/Validationservlet")
 public class Validationservlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
-     * @see HttpServlet#HttpServlet()
+     * In this Assignment, the login page navigates to the Validationservlet where the login id and password is validated and then it is 
+     * forwarded to the Responseservlet where we have used session, cookies 
+     * and RequestDispatcher and then it goes to Calculationservlet where the average, grades,
+     * minimum and maximum is calculated and the final result is then displayed on final.jsp 
      */
     public Validationservlet() {
         super();
@@ -31,11 +47,35 @@ public class Validationservlet extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * This validates the id and password for the login page
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		response.setContentType("text/html");  
+	    PrintWriter out = response.getWriter(); 
+	    
+	    String n1 = request.getParameter("userName");
+		//out.print("Welcome " + n1);
+		
+	    String n=request.getParameter("username");  
+	    String p=request.getParameter("password");
+	    
+	    
+	    if(n.equals("admin") && p.equals("123")) // admin is the username and 123 is the password required to login!
+	    		
+	    		{  
+	        RequestDispatcher rd=request.getRequestDispatcher("Responseservlet");  
+			HttpSession session = request.getSession();
+			session.setAttribute("uname", n);
+			rd.forward(request, response); 
+	        
+	    }  
+	    else{  
+	        out.print("Sorry UserName or Password Error!");  
+	        RequestDispatcher rd=request.getRequestDispatcher("/login.jsp");  
+	        rd.include(request, response);   
+	}
+
 	}
 
 }
